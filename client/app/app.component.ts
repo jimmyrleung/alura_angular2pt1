ebf318ddaa9e9bf5958ffb8e0ed25dac58a495c6
Jimmy Leung <jimmy.rl19@hotmail.com>
2017-10-18 20:57:07 -0200

    Curso Angular 2 Parte I

---
+++ client/app/app.component.ts
@@ -0,0 +1,13 @@
+// Component: É um decorator (é análogo a uma annotation)
+import { Component } from '@angular/core'; // Procura nos node_modules o @angular/core
+
+@Component({
+    selector: 'app', // Como iremos acessar nosso componente nas views (<app>),
+    templateUrl: './app/app.component.html' // A partir da raíz (client) busca o template html
+})
+
+class AppComponent {
+    constructor(parameters) {
+
+    }
+}
